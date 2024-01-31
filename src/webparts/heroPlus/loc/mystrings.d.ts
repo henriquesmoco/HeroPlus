103613b204d3eb8be2440949832a897038926702
Henrique Smoco <henriquesmoco@outlook.com>
2024-01-30 19:24:16 -0600

    Initial Import - Auto Generated Project

---
+++ src/webparts/heroPlus/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHeroPlusWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'HeroPlusWebPartStrings' {
+  const strings: IHeroPlusWebPartStrings;
+  export = strings;
+}
